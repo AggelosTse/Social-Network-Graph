596edbcca06c73f80f691a888c2a47a62acf3ed2
AggelosTse <aggu8914@gmail.com>
2024-12-23 18:30:57 -0500

    fixed removing node.

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from addnode import add
 from removenode import remove
 from neighbors import findneighbors
 from menu import printmenu
-
+import time
 
         
 # Initialize the graph as an adjacency list
 graph = {}
 while True:
-    
+   clear_screen()
+   
    answer = printmenu() 
    if answer == "1":
        displaygraph(graph)
    elif answer == "2":
        add(graph)      
    elif answer == "3":
-       remove(graph)            #prepei na valo if graph empty statement
+       if graph == {}:
+              print("You cant remove nodes. Graph is empty.\n")
+              time.sleep(2)
+       else:
+              remove(graph)            
    elif answer == "4":
-       findneighbors(graph)     #broken, i gotta fix it
+       findneighbors(graph)    
    elif answer == "5":
        with open("test.txt", "a") as myfile:
            myfile.write(str(graph) + "\n")
--- removenode.py
+++ removenode.py
@@ -1,9 +1,14 @@
 from clearscreen import clear_screen
-        
+import time
+
 def remove(graph):
+       
     removenode = input("Which node would you like to remove?\n").strip().lower()
     clear_screen()
     while removenode not in graph:
            removenode = input("Node not in graph. Give another node\n").strip().lower()
+           time.sleep(2)
            clear_screen()
-    del graph[removenode]   
+    del graph[removenode]  
+    print(f"{removenode} deleted successfully.") 
+    time.sleep(2)
